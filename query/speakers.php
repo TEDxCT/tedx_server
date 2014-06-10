b2671b7db89da056dc228aa6ed6266ac1acea8b9
Andrew Pettey <p3tdog@gmail.com>
2014-06-10 20:17:15 +0200

    Added sponsors response

--- query/speakers.php
+++ query/speakers.php
@@ -4,13 +4,12 @@ require($_SERVER['DOCUMENT_ROOT'].'/tedx_server/database/MySqlResponse.php');
 require($_SERVER['DOCUMENT_ROOT'].'/tedx_server/converter/speakers.php');
 function CreateJson()
 {
-    $sql = "SELECT * FROM speaker s INNER JOIN linkspeakerurl lsu ON s.id = lsu.speakerid INNER JOIN url u ON u.id = lsu.urlid";
+    $sql = "SELECT * FROM speaker";
 
     $response = MySqlResponse::getInstance();
     $result = $response::mySqlQuery($sql);
     $json = array();
     header('Content-type: application/json');
-    $data;
     if(mysql_num_rows($result)){
         while($row=mysql_fetch_array($result)){
             $object = new speakers($row);
